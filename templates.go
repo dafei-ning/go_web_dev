dc234a27ee5cd979d2395fefb083e81436cf85b7
Dafei Ning <ningdafei@gmail.com>
2020-01-09 08:39:03 -0800

    go web template2

--- templates.go
+++ templates.go
@@ -14,8 +14,21 @@ func init() {
 }
 
 func main() {
+	// 用Execute的os方法写出tpl.
 	err := tpl.Execute(os.Stdout, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	// 创建以index.html为文件名的文件nf.
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// 用Execute的方法将tpl内容写进nf.
+	err = tpl.Execute(nf, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer nf.Close()
+
 }
